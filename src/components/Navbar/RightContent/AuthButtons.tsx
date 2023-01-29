266799abb3b178e9154d99994ca3e463e426b56b
rabobahago <rabobahago@gmail.com>
2023-01-29 11:21:05 +0100

    global button chakra ui created

--- src/components/Navbar/RightContent/AuthButtons.tsx
+++ src/components/Navbar/RightContent/AuthButtons.tsx
@@ -3,8 +3,25 @@ import { Button } from "@chakra-ui/react";
 const AuthButtons = () => {
   return (
     <>
-      <Button>Log In</Button>
-      <Button>Sign Up</Button>
+      <Button
+        variant="outline"
+        height="28px"
+        display={{ base: "none", sm: "flex" }}
+        width={{ base: "70px", md: "110px" }}
+        mr={2}
+        // onClick={() => {}}
+      >
+        Log In
+      </Button>
+      <Button
+        height="28px"
+        display={{ base: "none", sm: "flex" }}
+        width={{ base: "70px", md: "110px" }}
+        mr={2}
+        // onClick={() => {}}
+      >
+        Sign Up
+      </Button>
     </>
   );
 };
