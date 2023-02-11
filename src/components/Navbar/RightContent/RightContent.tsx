d0328355a087958e6d99faaae0ba605059f91256
rabobahago <rabobahago@gmail.com>
2023-02-11 01:56:16 +0100

    logout button replaced with Icons component in the navbar

--- src/components/Navbar/RightContent/RightContent.tsx
+++ src/components/Navbar/RightContent/RightContent.tsx
@@ -1,12 +1,13 @@
 import AuthModal from "@/components/modal/Auth/AuthModal";
 import { auth } from "@/firebase/clientApp";
-import { Button, Flex } from "@chakra-ui/react";
-import { signOut } from "firebase/auth";
+import { Button, Flex, Icon } from "@chakra-ui/react";
+import { signOut, User } from "firebase/auth";
 import React from "react";
 import AuthButtons from "./AuthButtons";
+import Icons from "./Icons";
 
 type RightContextProps = {
-  user: any;
+  user?: User | null;
 };
 
 const RightContext: React.FC<RightContextProps> = ({ user }) => {
@@ -15,11 +16,8 @@ const RightContext: React.FC<RightContextProps> = ({ user }) => {
       <AuthModal />
 
       <Flex justify="center" align="center">
-        {user ? (
-          <Button onClick={() => signOut(auth)}>Logout</Button>
-        ) : (
-          <AuthButtons />
-        )}
+        {user ? <Icons /> : <AuthButtons />}
+        {/* <Menu/> */}
       </Flex>
     </>
   );
