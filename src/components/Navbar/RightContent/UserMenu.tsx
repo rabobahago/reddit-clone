90c0a8b878781d3be76aa765b305c2a02389f3b7
rabobahago <rabobahago@gmail.com>
2023-02-11 02:36:27 +0100

    profile menu added

--- src/components/Navbar/RightContent/UserMenu.tsx
+++ src/components/Navbar/RightContent/UserMenu.tsx
@@ -1,21 +1,57 @@
 import { ChevronDownIcon } from "@chakra-ui/icons";
-import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+import {
+  Button,
+  Flex,
+  Icon,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+} from "@chakra-ui/react";
+import { User } from "firebase/auth";
 import React from "react";
+import { FaRedditSquare } from "react-icons/fa";
+import { VscAccount } from "react-icons/vsc";
+import { IoSparkles } from "react-icons/io5";
+import { CgProfile } from "react-icons/cg";
+type UserMenuProps = {
+  user?: User | null;
+};
 
-type UserMenuProps = {};
-
-const UserMenu: React.FC<UserMenuProps> = () => {
+const UserMenu: React.FC<UserMenuProps> = ({ user }) => {
   return (
     <Menu>
-      <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
-        Actions
+      <MenuButton
+        cursor="pointer"
+        padding="0px 6px"
+        borderRadius={4}
+        _hover={{ outline: "1px solid", outlineColor: "gray.200" }}
+      >
+        {user ? (
+          <Flex align="center">
+            <Flex align="center">
+              <>
+                <Icon
+                  fontSize={24}
+                  mr={1}
+                  color="gray.300"
+                  as={FaRedditSquare}
+                />
+              </>
+              <ChevronDownIcon />
+            </Flex>
+          </Flex>
+        ) : (
+          <Icon fontSize={24} color="gray.400" mr={1} as={VscAccount} />
+        )}
       </MenuButton>
       <MenuList>
-        <MenuItem>Download</MenuItem>
-        <MenuItem>Create a Copy</MenuItem>
-        <MenuItem>Mark as Draft</MenuItem>
-        <MenuItem>Delete</MenuItem>
-        <MenuItem>Attend a Workshop</MenuItem>
+        <MenuItem>
+          <Flex>
+            <Icon as={CgProfile} />
+            Profile
+          </Flex>
+        </MenuItem>
       </MenuList>
     </Menu>
   );
