c693f21091d463ad1ca811c2dda8cbc99dcb04dd
rabobahago <rabobahago@gmail.com>
2023-02-13 03:48:59 +0100

    home navbar responsive css chakra ui added

--- src/components/Navbar/directory/Directory.tsx
+++ src/components/Navbar/directory/Directory.tsx
@@ -9,19 +9,27 @@ const UserMenu: React.FC = () => {
         cursor="pointer"
         padding="0px 6px"
         borderRadius={4}
+        mr={2}
+        ml={{ base: 0, md: 2 }}
         _hover={{ outline: "1px solid", outlineColor: "gray.200" }}
       >
-        <Flex align="center">
+        <Flex
+          align="center"
+          justify="space-between"
+          width={{ base: "auto", lg: "200px" }}
+        >
           <Flex align="center">
             <Icon fontSize={24} mr={{ base: 1, md: 2 }} as={TiHome} />
             <Flex display={{ base: "none", lg: "flex" }}>
-              <Text>Home</Text>
+              <Text fontWeight={600} fontSize="10pt">
+                Home
+              </Text>
             </Flex>
           </Flex>
           <ChevronDownIcon />
         </Flex>
       </MenuButton>
-      <MenuList></MenuList>
+      <MenuList>{/* <Communities/> */}Communities</MenuList>
     </Menu>
   );
 };
