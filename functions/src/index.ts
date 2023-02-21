2f60d2fdaccc31fc782c94a142b09c1bdaeee19d
rabobahago <rabobahago@gmail.com>
2023-02-22 00:57:46 +0100

    successfully deployed firebase functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,11 @@
 import * as functions from "firebase-functions";
-
-// // Start writing functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+import * as admin from "firebase-admin";
+admin.initializeApp();
+const db = admin.firestore();
+export const createUserDocument = functions.auth
+  .user()
+  .onCreate(async (user) => {
+    db.collection("users")
+      .doc(user.uid)
+      .set(JSON.parse(JSON.stringify(user)));
+  });
