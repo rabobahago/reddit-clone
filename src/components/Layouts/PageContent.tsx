64e901f84ad0e9fb81950d8ddce86bc39b919acc
rabobahago <rabobahago@gmail.com>
2023-03-06 08:46:23 +0100

    page content responsive style sheet completed

--- src/components/Layouts/PageContent.tsx
+++ src/components/Layouts/PageContent.tsx
@@ -5,12 +5,31 @@ type PageContentProps = {};
 
 const PageContent: React.FC<PageContentProps> = ({ children }) => {
   return (
-    <Flex>
-      <Flex>
+    <Flex justify="center" p="16px 0px" border="1px solid red">
+      <Flex
+        width="95%"
+        justify="center"
+        maxWidth="860px"
+        border="1px solid green"
+      >
         {/* RHS */}
-        <Flex>{children && children[0 as keyof typeof children]}</Flex>
+        <Flex
+          direction="column"
+          width={{ base: "100%", md: "65%" }}
+          mr={{ base: 0, md: 6 }}
+          border="1px solid blue"
+        >
+          {children && children[0 as keyof typeof children]}
+        </Flex>
         {/* LHS */}
-        <Flex>{children && children[1 as keyof typeof children]}</Flex>
+        <Flex
+          direction="column"
+          display={{ base: "none", md: "flex" }}
+          flexGrow={1}
+          border="1px solid orange"
+        >
+          {children && children[1 as keyof typeof children]}
+        </Flex>
       </Flex>
     </Flex>
   );
