d7794887f0999d02ba13f3152b523789c70057f3
rabobahago <rabobahago@gmail.com>
2023-03-15 08:14:32 +0100

    new post form created

--- src/pages/r/[communityId]/submit.tsx
+++ src/pages/r/[communityId]/submit.tsx
@@ -1,8 +1,19 @@
+import PageContent from "@/components/Layouts/PageContent";
+import NewPostForm from "@/components/posts/NewPostForm";
+import { Box, Text } from "@chakra-ui/react";
 import React from "react";
 
-type submitProps = {};
-
-const submit: React.FC<submitProps> = () => {
-  return <div>Have a good coding</div>;
+const SubmitPostPage: React.FC = () => {
+  return (
+    <PageContent>
+      <>
+        <Box p="14px 0px" borderBottom="1px solid" borderColor="white">
+          <Text>Create a Post</Text>
+        </Box>
+        <NewPostForm />
+      </>
+      <>{/* About */}</>
+    </PageContent>
+  );
 };
-export default submit;
+export default SubmitPostPage;
